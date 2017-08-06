468bb0f740dfd5bc3694dd1546c112f969566c59
Alexis ZRIHEN <zrihenalexis@gmail.com>
2017-08-07 00:45:24 +0200

    Navigate to nextPage if the request is executed successfully

--- src/scenes/Consultations/identification/actions.js
+++ src/scenes/Consultations/identification/actions.js
@@ -1,11 +1,14 @@
 import axios from '../../../axios.config'
 
-export function submitFormToAPI(values) {
+export function submitFormToAPI(values, nextPage, history, stepId) {
   		console.log('kokokokokookook')
+		values.endDate =  "2017-08-06T22:40:51.802Z";
   		const step1 = axios.post('u2m-api/v1/consultation/step1', values)
   		return (dispatch) =>(
 			step1.then(function (response) {
 				console.log('RESSSPPPP', response);
+				// navigateToNextStep();
+				nextPage(history, '/consultations/', stepId)
 			})
 			.catch(function (error) {
 				console.log('ERRRR', error);
