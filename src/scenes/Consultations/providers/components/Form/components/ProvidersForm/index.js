f1ea62fad380ef79cc8d885f9751ae4b9f93e628
Alexis ZRIHEN <zrihenalexis@gmail.com>
2017-09-13 00:39:07 +0200

    Fix typeahead, add validations fields

--- src/scenes/Consultations/providers/components/Form/components/ProvidersForm/index.js
+++ src/scenes/Consultations/providers/components/Form/components/ProvidersForm/index.js
@@ -19,6 +19,7 @@ import {
 import renderInput from '../../../../../../../components/Fields/input'
 import Select from '../../../../../../../components/Fields/select'
 import selectTypeahead from '../../../../../../../components/Fields/selectTypeahead'
+import asyncTypeAhead from '../../../../../../../components/Fields/asyncTypeAhead'
 import RangeTimePicker from '../../../../../../../components/RangeTimePicker/index'
 import SingleTimePicker from '../../../../../../../components/SingleTimePicker/index'
 import { isModalVisible } from '../../../../../../../components/Modal/actions'
@@ -26,7 +27,7 @@ import { Field, reduxForm } from 'redux-form'
 import './styles/style.css'
 const { DOM: { input } } = React
 
-const isSupplierExisting = (contacts, index) => contacts[index].supplier && contacts[index].supplier.length > 0;
+const isSupplierExisting = (contacts, index) => contacts[index].supplier.length > 0;
 
 const ProvidersForm = ({ handleSubmit, fields, addProvidersField, removeProvidersField, isModalVisible, suppliers, contacts, onSearch }) => (console.log('oKOKKKOK', onSearch),
 	<Col xs={12} md={12} lg={12} className="Providers">
@@ -36,18 +37,11 @@ const ProvidersForm = ({ handleSubmit, fields, addProvidersField, removeProvider
 				<Col xs={12} md={12} lg={12} key={index} className="fields">
 					<Row className="show-grid">
 				      	<Col sm={2} md={2} lg={4}>
-					      	<Field type="select" withButton withGlyph="plus" onClick={() => isModalVisible(true)} onSearch={onSearch}   options={suppliers} placeholder="Chercher fournisseur par nom" withoutLabel name={`consultationSupplierList[${index}].supplier`} component={selectTypeahead}>Chercher fournisseur par nom</Field>
+					      	<Field type="select" withButton withGlyph="plus" onClick={() => isModalVisible(true)} onSearch={onSearch}   options={suppliers} placeholder="Chercher fournisseur par nom" withoutLabel name={`consultationSupplierList[${index}].supplier`} component={asyncTypeAhead}>Chercher fournisseur par nom</Field>
 				      	</Col>
-				      	{/*<Col sm={2} md={2} lg={2}>
-				      		<Field type="text" placeholder="Adresse e-mail" withoutLabel name={`consultationSupplierList[${index}].mail`} component={renderInput}>Adresse e-mail</Field>
-				      	</Col>*/}
-				      	{console.log('okokkooo', contacts)}
-						{ <Col sm={2} md={2} lg={3}>
+						<Col sm={2} md={2} lg={3}>
 							<Field type="select" options={ isSupplierExisting(contacts, index) ? contacts[index].supplier[0].contactPersonList : []}  placeholder="Nom de l'interlocuteur" withoutLabel name={`consultationSupplierList[${index}].interlocutor`} component={selectTypeahead}>Nom de l'interlocuteur</Field>
-						</Col> }
-				      	{/*<Col sm={2} md={2} lg={2}>
-				      		<Field type="text" component={renderInput} withoutLabel name={`consultationSupplierList[${index}].job`} placeholder="Poste">Poste</Field>
-				      	</Col>*/}
+						</Col>
 				      	<Col sm={1} md={1} lg={1}>
 				      		<span>
 					          <Field name={`consultationSupplierList[${index}].referenceCustomerRequested`} id='client' component="input" normalize={(v) => { return _.isBoolean(v) ? v : false; }} type="checkbox"/>
