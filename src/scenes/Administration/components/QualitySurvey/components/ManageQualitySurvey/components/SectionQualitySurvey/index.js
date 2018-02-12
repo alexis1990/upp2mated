35f5d278bf6ad40583181670319a767830342f69
Nicolas Jamet <nicolas.jamet.pro@gmail.com>
2018-02-12 23:21:06 +0100

    [UP-161] eslint.

--- src/scenes/Administration/components/QualitySurvey/components/ManageQualitySurvey/components/SectionQualitySurvey/index.js
+++ src/scenes/Administration/components/QualitySurvey/components/ManageQualitySurvey/components/SectionQualitySurvey/index.js
@@ -8,41 +8,41 @@ import { required } from '../../../../../../../../utils/inputRules';
 import Question from '../QuestionQualitySurvey/';
 
 const Section = ({ fields, field, index, addChangeSetModify, addChangeSetRemove, types }) => {
-    const status = fields.getAll()[index].status || 'add';
+  const status = fields.getAll()[index].status || 'add';
 
-    return (
-        <li key={index} className={`sections section-status-${status.toLowerCase()}`}>
-            <div className="trash-row">
-                <Button
-                    type="button"
-                    bsStyle="btn btn-action-button font-icon"
-                    onClick={() => {
-                        fields.remove(index);
-                        addChangeSetRemove(index, types);
-                    }}
-                >
-                    <Glyphicon glyph="remove" />
-                </Button>
-            </div>
-            <Col lg={12} className={`section-status-${field.status}`}>
-                <Col lg={3}>
-                    <h4>Section {index + 1}</h4>
-                </Col>
-                <Col lg={4}>
-                    <Field
-                        name={`${field}.content`}
-                        type="text"
-                        withoutLabel
-                        component={renderInput}
-                        placeholder="Nom"
-                        onChange={(input) => addChangeSetModify(index, types)}
-                        validate={[required]}
-                    />
-                </Col>
-            </Col>
-            <FieldArray name={`${field}.questions`} parentId={index} dragSource="QUESTION" dropTarget="QUESTION" component={Question} />
-        </li>
-    );
+  return (
+    <li key={index} className={`sections section-status-${status.toLowerCase()}`}>
+      <div className="trash-row">
+        <Button
+          type="button"
+          bsStyle="btn btn-action-button font-icon"
+          onClick={() => {
+            fields.remove(index);
+            addChangeSetRemove(index, types);
+          }}
+        >
+          <Glyphicon glyph="remove" />
+        </Button>
+      </div>
+      <Col lg={12} className={`section-status-${field.status}`}>
+        <Col lg={3}>
+          <h4>Section {index + 1}</h4>
+        </Col>
+        <Col lg={4}>
+          <Field
+            name={`${field}.content`}
+            type="text"
+            withoutLabel
+            component={renderInput}
+            placeholder="Nom"
+            onChange={(input) => addChangeSetModify(index, types)}
+            validate={[required]}
+          />
+        </Col>
+      </Col>
+      <FieldArray name={`${field}.questions`} parentId={index} dragSource="QUESTION" dropTarget="QUESTION" component={Question} />
+    </li>
+  );
 };
 
 export default StaticBlockWrapperHOC(DraggableContainerWrapperHOC(Section));
