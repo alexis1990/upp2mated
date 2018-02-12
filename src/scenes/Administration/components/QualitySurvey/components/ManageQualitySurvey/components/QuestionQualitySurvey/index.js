5b3e41d7f651b55d6b369a31e42c79c917bdb30f
Nicolas 'Neodern' Jamet <nicolas.jamet.pro@gmail.com>
2018-02-12 13:44:57 +0100

    [UP-161] - Affichage du diff

--- src/scenes/Administration/components/QualitySurvey/components/ManageQualitySurvey/components/QuestionQualitySurvey/index.js
+++ src/scenes/Administration/components/QualitySurvey/components/ManageQualitySurvey/components/QuestionQualitySurvey/index.js
@@ -1,37 +1,41 @@
-import React from 'react'
-import { Col, Button, Glyphicon } from 'react-bootstrap'
-import { FieldArray, Field } from 'redux-form'
-import renderInput from '../../../../../../../../components/Fields/input'
-import StaticBlockWrapperHOC from '../../../../../../../../components/DraggableHOC/'
-import DraggableContainerWrapperHOC from '../../../../../../../../components/DraggableHOC/components/DraggableElement'
-import { required } from '../../../../../../../../utils/inputRules'
+import React from 'react';
+import { Col, Button, Glyphicon } from 'react-bootstrap';
+import { FieldArray, Field } from 'redux-form';
+import renderInput from '../../../../../../../../components/Fields/input';
+import StaticBlockWrapperHOC from '../../../../../../../../components/DraggableHOC/';
+import DraggableContainerWrapperHOC from '../../../../../../../../components/DraggableHOC/components/DraggableElement';
+import { required } from '../../../../../../../../utils/inputRules';
 
-const Question = ({ fields, field, index, dragSource, addChangeSetModify, addChangeSetRemove }) => (
-    <li key={index} className="question-row">
-        <div className="question-field">
-            <Field
-                name={`${field}.content`}
-                type="text"
-                component={renderInput}
-                label={`Question #${index + 1}`}
-                onChange={(input)=> addChangeSetModify(index, dragSource)}
-                validate={[ required ]}
-            />
-        </div>
-        <div className="add-question-button">
-            <Button
-                type="button"
-                bsStyle="btn btn-action-button font-icon"
-                onClick={() => {
-                        fields.remove(index)
-                        addChangeSetRemove(index, dragSource)
+const Question = ({ fields, field, index, dragSource, addChangeSetModify, addChangeSetRemove }) => {
+    const status = fields.getAll()[index].status || 'add';
+
+    return (
+        <li key={index} className={`question-row question-status-${status.toLowerCase()}`}>
+            <div className="question-field">
+                <Field
+                    name={`${field}.content`}
+                    type="text"
+                    component={renderInput}
+                    label={`Question #${index + 1}`}
+                    onChange={(input) => addChangeSetModify(index, dragSource)}
+                    validate={[required]}
+                />
+            </div>
+            <div className="add-question-button">
+                <Button
+                    type="button"
+                    bsStyle="btn btn-action-button font-icon"
+                    onClick={() => {
+                        fields.remove(index);
+                        addChangeSetRemove(index, dragSource);
                     }
-                }
-            >
-                <Glyphicon glyph="remove" />
-            </Button>
-        </div>
-    </li>
-)
+                    }
+                >
+                    <Glyphicon glyph="remove" />
+                </Button>
+            </div>
+        </li>
+    );
+};
 
 export default StaticBlockWrapperHOC(DraggableContainerWrapperHOC(Question));
--- src/scenes/Administration/components/QualitySurvey/components/ManageQualitySurvey/components/SectionQualitySurvey/index.js
+++ src/scenes/Administration/components/QualitySurvey/components/ManageQualitySurvey/components/SectionQualitySurvey/index.js
@@ -7,38 +7,42 @@ import DraggableContainerWrapperHOC from '../../../../../../../../components/Dra
 import { required } from '../../../../../../../../utils/inputRules';
 import Question from '../QuestionQualitySurvey/';
 
-const Section = ({ fields, field, index, addChangeSetModify, addChangeSetRemove, types }) => (
-  <li key={index} className="sections">
-    <div className="trash-row">
-      <Button
-        type="button"
-        bsStyle="btn btn-action-button font-icon"
-        onClick={() => {
-          fields.remove(index);
-          addChangeSetRemove(index, types);
-        }}
-      >
-        <Glyphicon glyph="remove" />
-      </Button>
-    </div>
-    <Col lg={12} className={`section-status-${field.status}`}>
-      <Col lg={3}>
-        <h4>Section {index + 1}</h4>
-      </Col>
-      <Col lg={4}>
-        <Field
-          name={`${field}.content`}
-          type="text"
-          withoutLabel
-          component={renderInput}
-          placeholder="Nom"
-          onChange={(input) => addChangeSetModify(index, types)}
-          validate={[required]}
-        />
-      </Col>
-    </Col>
-    <FieldArray name={`${field}.questions`} parentId={index} dragSource="QUESTION" dropTarget="QUESTION" component={Question} />
-  </li>
-);
+const Section = ({ fields, field, index, addChangeSetModify, addChangeSetRemove, types }) => {
+    const status = fields.getAll()[index].status || 'add';
+
+    return (
+        <li key={index} className={`sections section-status-${status.toLowerCase()}`}>
+            <div className="trash-row">
+                <Button
+                    type="button"
+                    bsStyle="btn btn-action-button font-icon"
+                    onClick={() => {
+                        fields.remove(index);
+                        addChangeSetRemove(index, types);
+                    }}
+                >
+                    <Glyphicon glyph="remove" />
+                </Button>
+            </div>
+            <Col lg={12} className={`section-status-${field.status}`}>
+                <Col lg={3}>
+                    <h4>Section {index + 1}</h4>
+                </Col>
+                <Col lg={4}>
+                    <Field
+                        name={`${field}.content`}
+                        type="text"
+                        withoutLabel
+                        component={renderInput}
+                        placeholder="Nom"
+                        onChange={(input) => addChangeSetModify(index, types)}
+                        validate={[required]}
+                    />
+                </Col>
+            </Col>
+            <FieldArray name={`${field}.questions`} parentId={index} dragSource="QUESTION" dropTarget="QUESTION" component={Question} />
+        </li>
+    );
+};
 
 export default StaticBlockWrapperHOC(DraggableContainerWrapperHOC(Section));
