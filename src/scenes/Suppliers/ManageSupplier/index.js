9eb465196f2d632f3ed26bf629ad72131310ec58
Nicolas Jamet <njamet-prestataire@oui.sncf>
2018-03-05 18:10:48 +0100

    [UP-167] - réapplication de mes modifs qui ont été écrasé lors du dernier merge ... meh.

--- src/scenes/Suppliers/ManageSupplier/index.js
+++ src/scenes/Suppliers/ManageSupplier/index.js
@@ -1,79 +1,72 @@
-import React, { Component } from 'react'
-import { Grid, Row, Col, Tab, Nav, NavItem } from 'react-bootstrap'
-import { connect } from 'react-redux'
-import { reduxForm } from 'redux-form'
-import { bindActionCreators } from 'redux'
-import FormSupplier from './components/FormSupplier/index'
-import FinancialHealth from './components/FinancialHealth'
-import { fetchSupplier } from '../actions'
-import { Link } from 'react-router-dom'
-import './styles/style.css'
+import React, { Component } from 'react';
+import { Col, Grid, Nav, NavItem, Row, Tab } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchSupplier } from '../actions';
+import ContactList from '../components/ContactList/index';
+import SupplierCardForm from './../components/SupplierCardForm';
+import FinancialHealth from './components/FinancialHealth';
+import './styles/style.css';
 
 class ManageSupplier extends Component {
-	constructor() {
-		super();
-		this.state = { contactPersonList: [] }
-	}
+  state = { contactPersonList: [] };
 
-	componentWillMount() {
-		const { match, fetchSupplier } = this.props;
-		const supplierId = match.params.id;
-		if (supplierId) {
-			fetchSupplier(supplierId)
-		}
-	}
+  componentWillMount() {
+    const { match, fetchSupplier } = this.props;
+    const supplierId = match.params.id;
+    if (supplierId) {
+      fetchSupplier(supplierId);
+    }
+  }
 
-	componentWillReceiveProps(nextProps) {
-		this.setState({ contactPersonList: nextProps.supplier.contactPersonList });
-	}
+  componentWillReceiveProps(nextProps) {
+    this.setState({ contactPersonList: nextProps.supplier.contactPersonList });
+  }
 
-	render() {
-		const { handleSubmit, supplier, match } = this.props;
-		const supplierId = match.params.id;
-		return (
-			<Grid fluid>
-				<Tab.Container id="left-tabs-example" defaultActiveKey="first">
-					<Row className="clearfix">
-						<Col sm={4}>
-							<h3> {supplierId ? `Fournisseur ${supplier.name}` : 'Nouveau Fournisseur'}</h3>
-							<Nav bsStyle="pills" stacked>
-								<NavItem eventKey="first">
-										Fiche du fournisseur
-								</NavItem>
-								<NavItem eventKey="second">
-									Santé financiére
-								</NavItem>
-							</Nav>
-						</Col>
-						<Col sm={8}>
-							<Tab.Content animation>
-								<Tab.Pane eventKey="first">
-									<FormSupplier contactPersonList={this.state.contactPersonList} />
-								</Tab.Pane>
-								<Tab.Pane eventKey="second">
-									<FinancialHealth supplierId={ supplierId } contactPersonList={this.state.contactPersonList} />
-								</Tab.Pane>
-							</Tab.Content>
-						</Col>
-					</Row>
-				</Tab.Container>
-			</Grid>
-		)
-	}
+  render() {
+    const { supplier, match } = this.props;
+    const supplierId = match.params.id;
+    return (
+      <Grid fluid>
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row className="clearfix">
+            <Col sm={4}>
+              <h3> {supplierId ? `Fournisseur ${supplier.name}` : 'Nouveau Fournisseur'}</h3>
+              <Nav bsStyle="pills" stacked>
+                <NavItem eventKey="first">
+                  Fiche du fournisseur
+                </NavItem>
+                <NavItem eventKey="second">
+                  Santé financiére
+                </NavItem>
+              </Nav>
+            </Col>
+            <Col sm={8}>
+              <Tab.Content animation>
+                <Tab.Pane eventKey="first">
+                  <SupplierCardForm />
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <FinancialHealth supplierId={supplierId} contactPersonList={this.state.contactPersonList} />
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  <ContactList suppliers={this.state.contactPersonList} />
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </Grid>
+    );
+  }
 }
 
-function mapStateToProps(state, ownProps) {
-	console.log('STATTEEESUPPP', state.form.Suppliers)
-	return {
-		supplier: state.form.Suppliers.supplier
-	};
-}
+const mapStateToProps = (state, ownProps) => ({
+  supplier: state.form.Suppliers.supplier,
+});
 
-function mapDispatchToProps(dispatch) {
-	return bindActionCreators({
-		fetchSupplier
-	}, dispatch);
-}
+const mapDispatchToProps = dispatch => bindActionCreators({
+  fetchSupplier,
+}, dispatch);
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(ManageSupplier)
+export default connect(mapStateToProps, mapDispatchToProps)(ManageSupplier);
--- src/scenes/Suppliers/index.js
+++ src/scenes/Suppliers/index.js
@@ -1,109 +1,108 @@
-import React, { Component } from 'react'
-import { Grid, Row, Col, Image } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { reduxForm } from 'redux-form'
-import { fetchSuppliers, deleteSupplier } from './actions'
-import { bindActionCreators } from 'redux'
-import { Table, ButtonGroup, Button, Glyphicon, Pagination } from 'react-bootstrap'
-import Spinner from '../../components/Spinner'
-import './styles/style.css'
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Col, Glyphicon, Grid, Pagination, Row, Table } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { deleteSupplier, fetchSuppliers } from './actions';
+import './styles/style.css';
 
 class Suppliers extends Component {
-	constructor() {
-		super();
-		this.state = {
-			activePage: 1
-		}
-		this.handleSelect = this.handleSelect.bind(this);
-	}
-	componentDidMount() {
-		const { fetchSuppliers } = this.props;
-		const { activePage } = this.state;
-		fetchSuppliers(activePage);
-	}
+  constructor() {
+    super();
+    this.state = {
+      activePage: 1,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
 
-	handleSelect(eventKey) {
-		const { fetchSuppliers } = this.props;
-		fetchSuppliers(eventKey);
-		this.setState({
-			activePage: eventKey
-		});
-	}
+  componentDidMount() {
+    const { fetchSuppliers } = this.props;
+    const { activePage } = this.state;
+    fetchSuppliers(activePage);
+  }
 
-	removeSupplier(supplierId){
-		const { deleteSupplier } = this.props;
-		deleteSupplier(supplierId)
-	}
+  handleSelect(eventKey) {
+    const { fetchSuppliers } = this.props;
+    fetchSuppliers(eventKey);
+    this.setState({
+      activePage: eventKey,
+    });
+  }
 
-	render() {
-		const { suppliers } = this.props;
+  removeSupplier(supplierId) {
+    const { deleteSupplier } = this.props;
+    deleteSupplier(supplierId);
+  }
 
-		return (
-			<Grid fluid>
-				<Row className="show-grid login-block">
-					<Col xs={12} md={12} lg={12} className="new_supplier">
-						<Link to="/suppliers/supplier/new">
-							<Button type="submit" bsStyle="action-button">
-								<Glyphicon glyph="plus" />Nouveau Fournisseur
-							 </Button>
-						</Link>
-					</Col>
-					<Col xs={12} md={12} lg={12} className="list-supliers list">
-						<Table responsive>
-							<thead>
-								<tr>
-									<th>Nom fournisseur</th>
-									<th>Core business</th>
-									<th>Technologie</th>
-									<th>Evaluation</th>
-									<th>Claims</th>
-									<th className="align-center">Actions</th>
-								</tr>
-							</thead>
-							<tbody>
-								{suppliers.content.map((supplier) => (
-									<tr>
-										<td width="20%">{supplier.name}</td>
-										<td width="20%">{supplier.subsidiary}</td>
-										<td width="10%"> Table cell</td>
-										<td width="10%">Table cell</td>
-										<td width="20%">Table cell</td>
-										<td className="actions" width="20%">
-											<ButtonGroup justified>
-												<Button className="action-button"><Link to={`/suppliers/${supplier.id}`}><Glyphicon glyph="eye-open" /></Link></Button>
-												<Button className="action-button"><Link to={`/suppliers/supplier/edit/${supplier.id}`}><Glyphicon glyph="pencil" /></Link></Button>
-												<Button className="action-button" onClick={() => this.removeSupplier(supplier.id)}><Glyphicon glyph="remove" /></Button>
-											</ButtonGroup>
-										</td>
-									</tr>
-								))}
-							</tbody>
-						</Table>
-						<Pagination
-							bsSize="medium"
-							items={suppliers.totalPages}
-							activePage={this.state.activePage}
-							onSelect={this.handleSelect} />
-					</Col>
-				</Row>
-			</Grid >
-		)
-	}
+  render() {
+    const { suppliers } = this.props;
+
+    console.log(suppliers);
+    return (
+      <Grid fluid>
+        <Row className="show-grid login-block">
+          <Col xs={12} md={12} lg={12} className="new_supplier">
+            <Link to="/suppliers/supplier/new">
+              <Button type="submit" bsStyle="action-button">
+                <Glyphicon glyph="plus" />Nouveau Fournisseur
+              </Button>
+            </Link>
+          </Col>
+          <Col xs={12} md={12} lg={12} className="list-supliers list">
+            <Table responsive>
+              <thead>
+              <tr>
+                <th>Nom fournisseur</th>
+                <th>Core business</th>
+                <th>Technologie</th>
+                <th>Evaluation</th>
+                <th>Claims</th>
+                <th className="align-center">Actions</th>
+              </tr>
+              </thead>
+              <tbody>
+              {suppliers.content.map((supplier) => (
+                <tr>
+                  <td width="20%">{supplier.name}</td>
+                  <td width="20%">{supplier.subsidiary}</td>
+                  <td width="10%"> Table cell</td>
+                  <td width="10%">Table cell</td>
+                  <td width="20%">Table cell</td>
+                  <td className="actions" width="20%">
+                    <ButtonGroup justified>
+                      <Button className="action-button"><Link to={`/suppliers/${supplier.id}`}><Glyphicon glyph="eye-open" /></Link></Button>
+                      <Button className="action-button"><Link to={`/suppliers/supplier/edit/${supplier.id}`}><Glyphicon glyph="pencil" /></Link></Button>
+                      <Button className="action-button" onClick={() => this.removeSupplier(supplier.id)}><Glyphicon glyph="remove" /></Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+            </Table>
+            <Pagination
+              bsSize="medium"
+              items={suppliers.totalPages}
+              activePage={this.state.activePage}
+              onSelect={this.handleSelect} />
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
 }
 
 function mapStateToProps(state, ownProps) {
-	return {
-		suppliers: state.form.Suppliers.suppliers
-	};
+  return {
+    suppliers: state.form.Suppliers.suppliers,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({
-		fetchSuppliers,
-		deleteSupplier
-	}, dispatch);
+  return bindActionCreators({
+    fetchSuppliers,
+    deleteSupplier,
+  }, dispatch);
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Suppliers)
+export default connect(mapStateToProps, mapDispatchToProps)(Suppliers);
