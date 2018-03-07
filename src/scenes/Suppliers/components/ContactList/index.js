f5b06b0a09f34593f3e2b3e68149be4a7fba5e53
Nicolas Jamet <nicolas.jamet.pro@gmail.com>
2018-03-07 20:30:41 +0100

    [UP-167] - Ajout du poste dans le tableau de contact. (+ PropTypes)

--- src/scenes/Suppliers/components/ContactList/index.js
+++ src/scenes/Suppliers/components/ContactList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { Table, Glyphicon, Button, ButtonGroup, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -6,29 +7,44 @@ import { Link } from 'react-router-dom';
 const ContactList = ({ suppliers, openModal }) => (
   <Table responsive>
     <thead>
-    <tr>
-      <th>Nom</th>
-      <th>Email</th>
-      <th className="align-center">Actions</th>
-    </tr>
+      <tr>
+        <th>Nom</th>
+        <th>Email</th>
+        <th>Poste</th>
+        <th className="align-center">Actions</th>
+      </tr>
     </thead>
     <tbody>
-    {suppliers.map(contact => (
-      <tr key={contact.id}>
-        <td width="40%">{contact.name}</td>
-        <td width="30%">{contact.email}</td>
-        <td className="actions" width="30%">
-          <ButtonGroup>
-            <Button className="action-button"><Link to={`/administration/roles/${contact.id}`}><Glyphicon glyph="eye-open" /></Link></Button>
-            <Button className="action-button" onClick={() => openModal(contact)}><Glyphicon glyph="pencil" /></Button>
-            <Button className="action-button"><Link to="/administration/roles/manage/authorizations"><Glyphicon glyph="cog" /></Link></Button>
-            <Button className="action-button" onClick={() => console.log('<<<<<<<<<<3')}><Glyphicon glyph="remove" /></Button>
-          </ButtonGroup>
-        </td>
-      </tr>
-    ))}
+      {suppliers.map(contact => (
+        <tr key={contact.id}>
+          <td>{contact.name}</td>
+          <td>{contact.email}</td>
+          <td>{contact.jobPosition}</td>
+          <td className="actions">
+            <ButtonGroup>
+              <Button className="action-button"><Link to={`/administration/roles/${contact.id}`}><Glyphicon glyph="eye-open" /></Link></Button>
+              <Button className="action-button" onClick={() => openModal(contact)}><Glyphicon glyph="pencil" /></Button>
+              <Button className="action-button"><Link to="/administration/roles/manage/authorizations"><Glyphicon glyph="cog" /></Link></Button>
+              <Button className="action-button" onClick={() => console.log('<<<<<<<<<<3')}><Glyphicon glyph="remove" /></Button>
+            </ButtonGroup>
+          </td>
+        </tr>
+      ))}
     </tbody>
   </Table>
 );
+
+ContactList.propTypes = {
+  suppliers: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    jobPosition: PropTypes.string,
+  })),
+};
+
+ContactList.defaultProps = {
+  suppliers: {},
+};
 
 export default ContactList;
